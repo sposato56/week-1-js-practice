8e22de7ec67d48d1fbc1264118f3ce945eff1608
John Sposato <you@example.combaytek2010@gmail.com>
2016-08-07 12:25:30 -0700

    add code

--- week-1/review-abstract-data-structures-challenge/abstract_data_structures.rb
+++ week-1/review-abstract-data-structures-challenge/abstract_data_structures.rb
@@ -1,7 +1,11 @@
 # 1. Describe
+# graph of a straight line
 
 # ------------
 # 2. Implement
+# Create method to populate a hash that has key value pairs 
+
 
 # ------------
 # 3. Get real
+# lot graph ???
