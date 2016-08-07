f495add1a94cb38291560e4e8275dd1062dc4547
John Sposato <you@example.combaytek2010@gmail.com>
2016-08-07 13:00:01 -0700

    final atempt

--- week-1/review-abstract-data-structures-challenge/abstract_data_structures.rb
+++ week-1/review-abstract-data-structures-challenge/abstract_data_structures.rb
@@ -3,9 +3,25 @@
 
 # ------------
 # 2. Implement
-# Create method to populate a hash that has key value pairs 
+# Create method to populate a hash that has key value pairs
+
+def graph(x, y)
+	n = 0
+	graph_h = Hash.new
+		while n < 20
+			graph_h[x] = y
+			n += 1
+			x +=1
+			y = (y*2)
+		end
+	p graph_h
+	graph_h.each(Key)
+	
+end 
 
 
+
+p graph(1,2)
 # ------------
 # 3. Get real
-# lot graph ???
+# plot graph ???
